e8fc5516a8a4973605d5d2f37ba757a2b1cca6db
Chris Smowton <chris@smowton.net>
2018-07-20 21:16:22 +0100

    First stab at a serializer

--- types/trace.py
+++ types/trace.py
@@ -1,4 +1,6 @@
 
+import serialize_trace.serializer as ser
+
 def l1norm(coord):
     assert len(coord) == 3
     return sum(map(abs, coord))
@@ -24,19 +26,30 @@ class Trace:
     class Halt:
         def __init__(self):
             pass
+        def serialize(self, stream):
+            stream.write(0b11111111)
 
     class Wait:
         def __init__(self):
             pass
+        def serialize(self, stream):
+            stream.write(0b11111110)
 
     class Flip:
         def __init__(self):
             pass
+        def serialize(self, stream):
+            stream.write(0b11111101)
 
     class SMove:
         def __init__(self, distance):
             self.distance = distance
             assert is_long_linear_difference(distance)
+        def serialize(self, stream):
+            ax = ser.get_linear_axis(self.distance)
+            mag = ser.get_long_linear_biased_magnitude(self.distance)
+            stream.write(0b00000100 & (ax << 4))
+            stream.write(mag)
 
     class LMove:
         def __init__(self, distance1, distance2):
@@ -44,24 +57,40 @@ class Trace:
             self.distance2 = distance2
             assert is_short_linear_distance(distance1)
             assert is_short_linear_distance(distance2)
+        def serialize(self, stream):
+            ax1 = ser.get_linear_axis(self.distance1)
+            mag1 = ser.get_long_linear_biased_magnitude(self.distance1)
+            ax2 = ser.get_linear_axis(self.distance2)
+            mag2 = ser.get_long_linear_biased_magnitude(self.distance2)
+            stream.write(0b00001100 & (ax2 << 6) & (ax1 << 6))
+            stream.write((mag2 << 4) & mag1)
 
     class FusionP:
         def __init__(self, distance):
             self.distance = distance
             assert is_near_difference(distance)
+        def serialize(self, stream):
+            stream.write(0b00000111 & (ser.get_near_difference_encoding(self.distance) << 5))
 
     class FusionS:
         def __init__(self, distance):
             self.distance = distance
             assert is_near_difference(distance)
+        def serialize(self, stream):
+            stream.write(0b00000110 & (ser.get_near_difference_encoding(self.distance) << 5))
 
     class Fission:
         def __init__(self, distance, seeds_kept):
             self.distance = distance
             self.seeds_kept = seeds_kept
             assert is_near_difference(distance)
+        def serialize(self, stream):
+            stream.write(0b00000101 & (ser.get_near_difference_encoding(self.distance) << 5))
+            stream.write(self.seeds_kept)
 
     class Fill:
         def __init__(self, distance):
             self.distance = distance
             assert is_near_difference(distance)
+        def serialize(self, stream):
+            stream.write(0b00000011 & (ser.get_near_difference_encoding(self.distance) << 5))
