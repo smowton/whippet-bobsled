acb397b36f67ef4d9bf7bb41d517856b097fb490
Peter West <peterjwest3@gmail.com>
2018-07-21 13:16:56 +0100

    Update pathfinding to prefer straight lines

--- bot_locomotion/pathfinding.py
+++ bot_locomotion/pathfinding.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-delta = [[-1, 0, 0],
-         [ 0,-1, 0],
-         [ 1, 0, 0],
-         [ 0, 1, 0],
-         [ 0, 0,-1],
-         [ 0, 0, 1]]
+directions = [[-1, 0, 0],
+             [ 0,-1, 0],
+             [ 1, 0, 0],
+             [ 0, 1, 0],
+             [ 0, 0,-1],
+             [ 0, 0, 1]]
 cost = 1
+
+def move_direction(x, y, z, direction, multiplier = 1):
+    x = x + directions[direction][0] * multiplier
+    y = y + directions[direction][1] * multiplier
+    z = z + directions[direction][2] * multiplier
+    return x, y, z
 
 def search(init, goal, grid):
     heuristic = calcHeuristic(grid, goal)
@@ -58,15 +64,22 @@ def search(init, goal, grid):
             if x == goal[0] and y == goal[1] and z == goal[2]:
                 found = True
             else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    z2 = z + delta[i][2]
+                for i in range(len(directions)):
+                    x2, y2, z2 = move_direction(x, y, z, i)
 
                     if z2 >= 0 and z2 < zdim and y2 >=0 and y2 < ydim and x2 >=0 and x2 < xdim:
                         if closed[x2,y2,z2] == 0 and not grid[x2,y2,z2]:
+
+                            continuation = False
+                            for j in range(len(directions)):
+                                x3, y3, z3 = move_direction(x2, y2, z2, j)
+                                if (x3 >= 0 and x3 < xdim and y3 >= 0 and y3 < ydim and z3 >= 0 and z3 < zdim):
+                                    x4, y4, z4 = move_direction(x2, y2, z2, action[x3, y3, z3])
+                                    if x2 == x4 and y2 == y4 and z2 == z4:
+                                        continuation = True
+
                             g2 = g + cost
-                            f2 = g2 + heuristic[x2,y2,z2]
+                            f2 = g2 + heuristic[x2,y2,z2] + (0 if continuation else 10)
                             openl.append([f2, g2, x2, y2, z2])
                             closed[x2,y2,z2] = 1
                             action[x2,y2,z2] = i
@@ -77,13 +90,8 @@ def search(init, goal, grid):
     path.append([goal[0], goal[1], goal[2]])
 
     while x != init[0] or y != init[1] or z != init[2]:
-        x2 = x-delta[action[x,y,z]][0]
-        y2 = y-delta[action[x,y,z]][1]
-        z2 = z-delta[action[x,y,z]][2]
-        x = x2
-        y = y2
-        z = z2
-        path.append([x2, y2, z2])
+        x, y, z = move_direction(x, y, z, action[x, y, z], -1)
+        path.append([x, y, z])
 
     path.reverse()
     return path
@@ -99,9 +107,7 @@ def calcHeuristic(grid, goal):
     for z in range(zdim):
         for y in range(ydim):
             for x in range(xdim):
-                dist=((x-goal[0])**2+(y-goal[1])**2+(z-goal[2])**2)**(1/2.0)
-                yheu = np.abs(float(y) - goal[1])
-                heuristic[x,y,z]= dist + yheu
+                heuristic[x, y, z] = (x - goal[0]) + (y - goal[1]) + (z - goal[2])
     return heuristic
 
 # start = [1, 50, 10]
