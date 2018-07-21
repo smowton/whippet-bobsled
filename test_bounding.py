62dfec426b22b0cef46980564595bb85854d967f
Thomas Spriggs <thomas.spriggs@diffblue.com>
2018-07-21 12:23:32 +0100

    Add `slice_is_empty` test

---
+++ test_bounding.py
@@ -0,0 +1,12 @@
+import model_reader.model_reader as model_reader
+import model_reader.model_functions as model_functions
+
+grid = model_reader.read('./data/problems/LA001_tgt.mdl')
+
+slice = grid[:, :, 1]
+model_reader.dump_slice(slice)
+print( model_functions.slice_is_empty(slice) )
+
+slice = grid[:, :, 19]
+model_reader.dump_slice(slice)
+print( model_functions.slice_is_empty(slice) )
