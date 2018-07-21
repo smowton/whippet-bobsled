493d779f3d7cab7e4564bf87b81e926b7a1aadd5
Peter West <peterjwest3@gmail.com>
2018-07-21 19:04:54 +0100

    Add force option to override existing file

--- make_simple_trace.py
+++ make_simple_trace.py
@@ -8,11 +8,11 @@ import model_reader.model_reader as mdl
 import trace_generators.simple_trace_generator as gen
 import serialize_trace.serializer as ser
 
-if len(sys.argv) != 3:
-    print("Usage: make_simple_trace.py some.mdl some.nbt", file = sys.stderr)
+if len(sys.argv) < 3:
+    print("Usage: make_simple_trace.py some.mdl some.nbt [-f]", file = sys.stderr)
     sys.exit(1)
 
-if os.path.exists(sys.argv[2]):
+if sys.argv[3] != '-f' and os.path.exists(sys.argv[2]):
     print("%s already exists" % sys.argv[2], file = sys.stderr)
     sys.exit(1)
 
