6cc86d2bb719bcfdc22f06cfd5a900c981698fcf
Peter West <peterjwest3@gmail.com>
2018-07-21 11:42:53 +0100

    Change model reader to export functions rather than CLI

--- model_reader/model_reader.py
+++ model_reader/model_reader.py
@@ -1,49 +1,49 @@
 import sys
 import numpy
 
-if len(sys.argv) != 2:
-    print('Usage - model_reader.py <model file>')
-    exit()
+def read(input_file_name):
+    print('Reading file "' + input_file_name + '"')
+    input_file = open(input_file_name, 'rb')
+    try:
+        resolution =  ord(input_file.read(1))
+        print('Resolution is ' + str(resolution))
+        bit_queue = []
+        grid = numpy.empty((resolution, resolution, resolution), bool)
+        for z in range(0, resolution):
+            for y in range(0, resolution):
+                for x in range(0, resolution):
+                    if len(bit_queue) == 0:
+                        byte = ord(input_file.read(1))
+                        for i in range(0,8):
+                            bit_queue.append((byte >> i) & 1)
+                    bit = bit_queue.pop(0)
+                    grid[x, y, z] = bit
+    finally:
+        input_file.close()
 
-input_file_name = sys.argv[1]
-print('Reading file "' + input_file_name + '"')
-input_file = open(input_file_name, 'rb')
-try:
-    resolution = ord(input_file.read(1))
-    print('Resolution is ' + str(resolution))
-    bit_queue = []
-    grid = numpy.empty((resolution, resolution, resolution), bool)
-    for z in range(0, resolution):
-        for y in range(0, resolution):
-            for x in range(0, resolution):
-                if len(bit_queue) == 0:
-                    byte = ord(input_file.read(1))
-                    for i in range(0,8):
-                        bit_queue.append((byte >> i) & 1)
-                bit = bit_queue.pop(0)
-                grid[x, y, z] = bit
-finally:
-    input_file.close()
-
+    return grid
 
 def dump_grid(grid):
+    resolution=grid.shape[0]
     for z in range(0, resolution):
         for y in range(0, resolution):
             for x in range(0, resolution):
                 print("x:"+str(x) + " y:"+str(y)+ " z:"+str(z), grid[x,y,z])
 
-def dump_slices(grid_slice):
-    for z in range(0, resolution):
-        print("slice "+ str(z))
+def dump_slices(grid, path = []):
+    resolution=grid.shape[0]
+    path_set = set((i[0], i[1], i[2]) for i in path)
+    print path_set
+    for y in range(0, resolution):
+        print("slice y="+ str(y))
         for x in range(0, resolution):
-            for y in range(0, resolution):
-                if (grid[x, z, y]):
+            for z in range(0, resolution):
+                if (grid[x, y, z]):
                     output_char = 'x'
+                elif ((x, y, z) in path_set):
+                    output_char = '#'
                 else:
                     output_char = '.'
                 sys.stdout.write(output_char)
             sys.stdout.write('\n')
         sys.stdout.flush()
-
-dump_grid(grid)
-dump_slices(grid)
