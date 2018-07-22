085bf59051929538e2cb18ed7f878ba05c645d72
Chris Smowton <chris@smowton.net>
2018-07-22 16:14:58 +0100

    Parallel generator: use irregular grid

--- trace_generators/parallel_trace_generator.py
+++ trace_generators/parallel_trace_generator.py
@@ -7,8 +7,50 @@ import simple_move as move
 import print_from_above_trace_generator as pfa
 import datatypes.trace as tr
 
+def equal_split_indices(histogram, nsplits):
+    total = sum(histogram)
+
+    result = [0]
+
+    target = total / nsplits
+
+    running_total = 0
+    for (idx, bar) in enumerate(histogram):
+        running_total += bar
+        if running_total >= target:
+            result.append(idx)
+            if len(result) == nsplits:
+                break
+            running_total = 0
+
+    assert len(result) <= nsplits + 1
+    while len(result) < nsplits + 1:
+        result.append(len(histogram))
+
+    # Normalize: ensure every region has width at least 1.
+    for idx in range(nsplits, 0, -1):
+        result[idx] = min(result[idx], len(histogram) - (nsplits - idx))
+
+    return result
+
+def split_by_equal_matter(model, xsplits, zsplits):
+
+    res = len(model)
+    yz_planes = [sum(model[x,y,z] for y in range(res) for z in range(res)) for x in range(res)]
+    xy_planes = [sum(model[x,y,z] for y in range(res) for x in range(res)) for z in range(res)]
+
+    assert sum(xy_planes) == sum(yz_planes)
+
+    xresult = equal_split_indices(yz_planes, xsplits)
+    zresult = equal_split_indices(xy_planes, zsplits)
+
+    assert len(xresult) == xsplits + 1
+    assert len(zresult) == zsplits + 1
+
+    return (xresult, zresult)
+
 class Bot:
-    def __init__(self, split_ops_queue, region_origin, region_size, pos, seeds, bots):
+    def __init__(self, split_ops_queue, region_origin, region_size, pos, seeds, bots, split_points):
         self.trace = tr.Trace()
         self.split_ops_queue = split_ops_queue
         self.region_origin = region_origin
@@ -16,6 +58,7 @@ class Bot:
         self.pos = pos # Relative to region_origin
         self.seeds = seeds
         self.bots = bots # back-pointer to the all-bots list
+        self.split_points = split_points # mapping from grid indices to voxels per axis
 
     def make_init_trace(self):
         while len(self.split_ops_queue) > 0:
@@ -28,16 +71,25 @@ class Bot:
             else:
                 split_ax = 2
 
-            # 1:1 or 2:1 ratio?
+            # 1:1 or 2:1 ratio (in cells)?
+            origin_in_cells = self.region_origin[split_ax]
+            origin_in_voxels = self.split_points[split_ax][origin_in_cells]
+
             if split.endswith("/2"):
-                split_point = self.region_size[split_ax] / 2
+                split_cell = self.region_size[split_ax] / 2
             else:
-                split_point = (2 * self.region_size[split_ax]) / 3
+                split_cell = (2 * self.region_size[split_ax]) / 3
+
+            # This is relative to this bot's origin:
+            split_voxel = \
+                self.split_points[split_ax][split_cell + self.region_origin[split_ax]] - \
+                origin_in_voxels
 
             # Are we above or below the line? This points towards the
             # new bot we'll spawn
-            split_sign = -1 if self.pos[split_ax] >= split_point else 1
+            split_sign = -1 if self.pos[split_ax] >= split_voxel else 1
 
+            # Seed distribution is independent of grid cell size:
             if split.endswith("/2"):
                 give_seeds = len(self.seeds) / 2
             else:
@@ -48,7 +100,7 @@ class Bot:
 
             # Move towards the split line:
             target_coord = list(self.pos)
-            target_coord[split_ax] = split_point
+            target_coord[split_ax] = split_voxel
             if split_sign == 1:
                 target_coord[split_ax] -= 1
             move.move(tr.coord_subtract(target_coord, self.pos), self.trace)
@@ -73,19 +125,20 @@ class Bot:
                         [self.split_ops_queue[0]] + \
                         [x for x in self.split_ops_queue[1:] if x != skip_op]
 
+            # Note all of these are measured in cells:
             new_bot_region_size = list(self.region_size)
             own_altered_region_size = list(self.region_size)
             new_bot_region_origin = list(self.region_origin)
             own_altered_region_origin = list(self.region_origin)
 
             if split_sign == 1:
-                new_bot_region_size[split_ax] -= split_point
-                own_altered_region_size[split_ax] = split_point
-                new_bot_region_origin[split_ax] += split_point
+                new_bot_region_size[split_ax] -= split_cell
+                own_altered_region_size[split_ax] = split_cell
+                new_bot_region_origin[split_ax] += split_cell
             else:
-                new_bot_region_size[split_ax] = split_point
-                own_altered_region_size[split_ax] -= split_point
-                own_altered_region_origin[split_ax] += split_point
+                new_bot_region_size[split_ax] = split_cell
+                own_altered_region_size[split_ax] -= split_cell
+                own_altered_region_origin[split_ax] += split_cell
 
             self.region_size = tuple(own_altered_region_size)
             self.region_origin = tuple(own_altered_region_origin)
@@ -93,14 +146,14 @@ class Bot:
             new_bot_pos = list(tr.coord_add(self.pos, fission_place))
             # Make the bots' positions own-region-relative:
             if split_sign == 1:
-                new_bot_pos[split_ax] -= split_point
+                new_bot_pos[split_ax] -= split_voxel
             else:
-                self.pos[split_ax] -= split_point
+                self.pos[split_ax] -= split_voxel
             new_bot_seeds = self.seeds[1:(give_seeds + 1)]
             self.seeds = self.seeds[(give_seeds + 1):]
 
             self.bots[new_bot_id] = \
-                Bot(new_bot_split_ops, tuple(new_bot_region_origin), tuple(new_bot_region_size), tuple(new_bot_pos), new_bot_seeds, self.bots)
+                Bot(new_bot_split_ops, tuple(new_bot_region_origin), tuple(new_bot_region_size), tuple(new_bot_pos), new_bot_seeds, self.bots, self.split_points)
             self.bots[new_bot_id].make_init_trace()
 
             self.split_ops_queue = self.split_ops_queue[1:]
@@ -112,25 +165,36 @@ def build_parallel_trace(model):
 
     res = len(model)
 
+    split_points_x, split_points_z = split_by_equal_matter(model, 6, 6)
+
     # Make the prelude: repeatedly fission until we have 6x6 bots each in their own region.
     toplevel_split_ops = ["x/2", "z/2", "x2:1", "z2:1", "x/2", "z/2"]
 
     bots = [None for i in range(40)]
-    bots[0] = Bot(toplevel_split_ops, (0, 0, 0), (res, res, res), (0, 0, 0), range(1, 40), bots)
+    bots[0] = Bot(toplevel_split_ops, (0, 0, 0), (6, 1, 6), (0, 0, 0), range(1, 40), bots, (split_points_x, [], split_points_z))
     bots[0].trace.add(tr.Trace.Flip())
     bots[0].make_init_trace()
 
+    for (idx, b) in enumerate(bots):
+        if b is not None:
+            # Now that regions are specified in cells, not voxels, each bot should be
+            # commanding a single cell.
+            assert b.region_size == (1, 1, 1)
+
+
+    # Indexed by cells, not voxels:
     bots_by_origin = {b.region_origin: idx for (idx, b) in enumerate(bots) if b is not None}
-    bots_by_xlim = {(b.region_origin[0] + b.region_size[0], b.region_origin[1], b.region_origin[2]): idx for (idx, b) in enumerate(bots) if b is not None}
-    bots_by_zlim = {(b.region_origin[0], b.region_origin[1], b.region_origin[2] + b.region_size[2]): idx for (idx, b) in enumerate(bots) if b is not None}
 
     # Have each bot do a simple print-from-above routine:
     for bot in bots:
         if bot is None:
             continue
-        submodel = model[bot.region_origin[0] : bot.region_origin[0] + bot.region_size[0], \
-                         0 : res, \
-                         bot.region_origin[2] : bot.region_origin[2] + bot.region_size[2]]
+        x_range_voxels = slice(split_points_x[bot.region_origin[0]], \
+                               split_points_x[bot.region_origin[0] + 1])
+        z_range_voxels = slice(split_points_z[bot.region_origin[2]], \
+                               split_points_z[bot.region_origin[2] + 1])
+
+        submodel = model[x_range_voxels, 0 : res, z_range_voxels]
 
         # Add build instructions for this region:
         simple_trace_gen = pfa.PrintFromAboveTraceBuilder(submodel, False)
@@ -140,30 +204,34 @@ def build_parallel_trace(model):
         simple_trace_gen.move_to((simple_trace_gen.pos[0], res - 1, simple_trace_gen.pos[2]))
 
         # If this isn't the rightmost bot, go to the edge to pick up the right-hand bot:
-        if bot.region_origin[0] + bot.region_size[0] != res:
-            simple_trace_gen.move_to((bot.region_size[0] - 1, res - 1, 0))
-            target_bot = bots_by_origin[(bot.region_origin[0] + bot.region_size[0], 0, bot.region_origin[2])]
+        if bot.region_origin[0] != 5:
+            bot_x_cell = bot.region_origin[0]
+            region_width_in_voxels = split_points_x[bot_x_cell + 1] - split_points_x[bot_x_cell]
+            simple_trace_gen.move_to((region_width_in_voxels - 1, res - 1, 0))
+            target_bot = bots_by_origin[(bot.region_origin[0] + 1, 0, bot.region_origin[2])]
             # This instruction will be properly synchronised later.
             simple_trace_gen.trace.instructions.append(tr.Trace.FusionP((1, 0, 0), target_bot))
 
         # If this isn't the leftmost bot, go to the back-right corner to get picked up:
         if bot.region_origin[0] != 0:
             simple_trace_gen.move_to((0, res - 1, 0))
-            target_bot = bots_by_xlim[bot.region_origin]
+            target_bot = bots_by_origin[(bot.region_origin[0] - 1, 0, bot.region_origin[2])]
 
             simple_trace_gen.trace.instructions.append(tr.Trace.FusionS((-1, 0, 0), target_bot))
 
         # If this isn't the backmost bot, go to the edge to pick up the bot behind:
-        if bot.region_origin[2] + bot.region_size[2] != res:
-            simple_trace_gen.move_to((0, res - 1, bot.region_size[2] - 1))
-            target_bot = bots_by_origin[(bot.region_origin[0], 0, bot.region_origin[2] + bot.region_size[2])]
+        if bot.region_origin[2] != 5:
+            bot_z_cell = bot.region_origin[2]
+            region_depth_in_voxels = split_points_z[bot_z_cell + 1] - split_points_z[bot_z_cell]
+            simple_trace_gen.move_to((0, res - 1, region_depth_in_voxels - 1))
+            target_bot = bots_by_origin[(bot.region_origin[0], 0, bot.region_origin[2] + 1)]
             # This instruction will be properly synchronised later.
             simple_trace_gen.trace.instructions.append(tr.Trace.FusionP((0, 0, 1), target_bot))
 
         # If this isn't the frontmost bot, go to the front corner to get picked up:
         if bot.region_origin[2] != 0:
             simple_trace_gen.move_to((0, res - 1, 0))
-            target_bot = bots_by_zlim[bot.region_origin]
+            target_bot = bots_by_origin[(bot.region_origin[0], 0, bot.region_origin[2] - 1)]
 
             simple_trace_gen.trace.instructions.append(tr.Trace.FusionS((0, 0, -1), target_bot))
 
