ba2c38c1d96cf6dc718da645db838a3e68b9de10
Peter West <peterjwest3@gmail.com>
2018-08-03 12:44:53 +0100

    Revert test_bounding file

--- test_bounding.py
+++ test_bounding.py
@@ -1,14 +1,14 @@
-import sys
-
 import model_reader.model_reader as model_reader
 import model_reader.model_functions as model_functions
-import trace_generators.layer_floodfill_generator as layer_floodfill_generator
-import serialize_trace.serializer as serializer
-import bot_locomotion.pathfinding as pathfinding
 
-model = model_reader.read('./data/problems/FA047_tgt.mdl')
+grid = model_reader.read('./data/problems/LA001_tgt.mdl')
 
-commands = layer_floodfill_generator.build_trace(model)
+slice = grid[:, :, 1]
+model_reader.dump_slice(slice)
+print( model_functions.slice_is_empty(slice) )
 
-with open(sys.argv[1], "w") as f:
-    serializer.serialize(commands, f)
+slice = grid[:, :, 19]
+model_reader.dump_slice(slice)
+print( model_functions.slice_is_empty(slice) )
+
+print(model_functions.bounds(grid))
